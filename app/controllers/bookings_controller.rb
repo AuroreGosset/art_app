4325f7ff5edb1387986545fd56d1d2ecdd15a50c
Fiona Martin <fionamartin03@gmail.com>
2022-02-22 18:00:58 +0100

    Booking create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,22 +2,26 @@ class BookingsController < ApplicationController
 
 
   def new
-    # @artwork = Artwork.find(params[:artwork_id])
-    # @booking = Booking.new
+    @artwork = Artwork.find(params[:artwork_id])
+    @booking = Booking.new
   end
 
-
   def create
-    # @booking = Booking.new(booking_params)
-    # @artwork = Artwork.find(params[:artwork_id])
-    # @booking.artwork = @artwork
-    # @booking.save
-    # redirect_to artwork_path(@artwork)
+    @booking = Booking.new(booking_params)
+    @artwork = Artwork.find(params[:artwork_id])
+    @booking.artwork = @artwork
+    @booking.user = current_user
+
+    if @booking.save!
+      redirect_to artwork_path(@artwork)
+    else
+      render :new
+    end
   end
 
   private
 
-  # def booking_params
-  #   params.require(:booking).permit(:start_date, :end_date)
-  # end
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
