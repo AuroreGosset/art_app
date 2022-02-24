9c58530a006fb83db98def7aa645fd475a0b6306
Fiona Martin <fionamartin03@gmail.com>
2022-02-24 17:35:10 +0100

    Artist Profil

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :artworks do
     resources :bookings, only: [:new, :create]
   end
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'dashboard', to: 'pages#dashboard'
 end
