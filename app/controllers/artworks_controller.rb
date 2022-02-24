1e54ad8c86d2457e957cd9555b915e429cc09616
Caroline Dana <caroline.m.dana@gmail.com>
2022-02-24 12:52:03 +0100

    début de la search, quelques mosifs sur la page index, liens sur dashboard cliquables, nouvelle page créer un artwork

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -6,4 +6,24 @@ class ArtworksController < ApplicationController
   def show
     @artwork = Artwork.find(params[:id])
   end
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.user = current_user
+    if @artwork.save
+      redirect_to dashboard_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:username, :title, :price_per_day, :description, :photo)
+  end
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,7 +1,16 @@
 class Artwork < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-
   has_one_attached :photo
   validates :title, :price_per_day, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    associated_against: {
+      user: [ :username ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
